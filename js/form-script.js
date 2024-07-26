691e336b88890514b6a71ca5c4706a40580a8892
arun909 <arunbabu7591@gmail.com>
2024-07-27 05:22:08 +0530

    pop message changed

--- js/form-script.js
+++ js/form-script.js
@@ -4,7 +4,7 @@ const form = document.forms['contact-form']
 form.addEventListener('submit', e => {
  e.preventDefault()
  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
- .then(response => alert("Thank you! your form is submitted successfully." ))
+ .then(response => alert("Thanks for reaching out!" ))
  .then(() => { window.location.reload(); })
  .catch(error => console.error('Error!', error.message))
 })
